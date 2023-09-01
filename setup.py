6365fb587c1237c5441be06ea55f4c1bb41dc924
Andrew Freiburger <freiburgermsu@aol.com>
2023-09-01 14:43:24 -0500

    setup update and example models

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     keywords = ['microbiology', "community", "scores", "interaction", "syntrophy", "competition"],
     # dependency_links = ['https://github.com/freiburgermsu/ModelSEEDpy/tarball/master#egg=modelseedpy-0.3.2'],
     install_requires=[
-        "modelseedpy_freiburgermsu >= 0.3.1.3",
+        "modelseedpy_freiburgermsu >= 0.3.1.4",
         "optlang", "numpy", "deepdiff", "sigfig"
     ],
     project_urls={
